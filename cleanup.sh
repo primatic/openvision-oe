57dbe67dea31266a5bf0e5092491fc9a2e44f7d6
persianpros <persianpros@yahoo.com>
2019-01-26 10:28:39 +0330

    Use another way to find adb ads files which could be faster

--- cleanup.sh
+++ cleanup.sh
@@ -14,50 +14,34 @@ echo ""
 echo -e "Removing ${GREEN}$MACHINE${NC} build folders, please wait ..."
 cd ..
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/buildstats | cut -f1
 rm -rf build/tmp/buildstats/*
 echo ""
 echo "buildstats cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/cache/default-glibc/$MACHINE | cut -f1
 rm -rf build/tmp/cache/default-glibc/$MACHINE
 echo ""
 echo "cache/default-glibc cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/deploy/ipk/$MACHINE | cut -f1
 rm -rf build/tmp/deploy/ipk/$MACHINE
 echo ""
 echo "deploy/ipk cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/deploy/images/$MACHINE | cut -f1
 rm -rf build/tmp/deploy/images/$MACHINE
 echo ""
 echo "deploy/images cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/log/cooker/$MACHINE | cut -f1
 rm -rf build/tmp/log/cooker/$MACHINE
 echo ""
 echo "log/cooker cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/pkgdata/$MACHINE | cut -f1
 rm -rf build/tmp/pkgdata/$MACHINE
 echo ""
 echo "pkgdata cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#echo "less than 10M"
 rm -rf build/tmp/sstate-control/*$MACHINE*
 echo ""
 echo "sstate-control cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/stamps/$MACHINE-oe-linu* | cut -f1
 rm -rf build/tmp/stamps/$MACHINE-oe-linu*
 echo ""
 echo "stamps cleaned!"
@@ -70,36 +54,28 @@ cd ..
 echo ""
 echo "packagedata_setscene cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/sysroots-components/$MACHINE | cut -f1
 rm -rf build/tmp/sysroots-components/$MACHINE
 echo ""
 echo "sysroots-components cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/work/$MACHINE-oe-linu* | cut -f1
 rm -rf build/tmp/work/$MACHINE-oe-linu*
 echo ""
 echo "work cleaned!"
 echo ""
 cd build/tmp/work
-find -name "*_$MACHINE_*.adb" -type f -exec rm -f {} \;
-find -name "*_$MACHINE_*.ads" -type f -exec rm -f {} \;
+find \( -iname \*_$MACHINE_*.adb -o -iname \*_$MACHINE_*.ads \) -type f -exec rm -f {} \;
 cd ..
 cd ..
 cd ..
 echo ""
 echo "work's adb ads files cleaned!"
 echo ""
-#echo "Space will be freed after this operation:"
-#du -sh build/tmp/work-shared/$MACHINE | cut -f1
 rm -rf build/tmp/work-shared/$MACHINE
 echo ""
 echo "work-shared cleaned!"
 echo ""
 cd build/tmp/work-shared
-find -name "*_$MACHINE_*.adb" -type f -exec rm -f {} \;
-find -name "*_$MACHINE_*.ads" -type f -exec rm -f {} \;
+find \( -iname \*_$MACHINE_*.adb -o -iname \*_$MACHINE_*.ads \) -type f -exec rm -f {} \;
 cd ..
 cd ..
 cd ..
