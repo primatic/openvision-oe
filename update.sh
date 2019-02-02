04228d3ee42a2c9dae71ff122d7e44ceb305af54
persianpros <persianpros@yahoo.com>
2019-02-02 15:11:03 +0330

    Get rid of old vuplus plugins in PLi's OE

--- update.sh
+++ update.sh
@@ -76,3 +76,5 @@ do
 done
 echo "BBLAYERS_append = \" ${SCRIPTPATH}\"" >> ${BUILDDIR}/conf/bblayers.conf
 rm -rf meta-openpli/recipes-extended/tzdata
+rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnabrowser.bb
+rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnaserver.bb
