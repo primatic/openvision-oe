105fcf2b40e96a62b894a84203c8f6ce71f2db00
Persian Prince <persianpros@yahoo.com>
2019-06-11 23:57:29 +0430

    Update update.sh

--- update.sh
+++ update.sh
@@ -278,4 +278,3 @@ rm -rf meta-openpli/recipes-extended/tzdata
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnabrowser.bb
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnaserver.bb
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-lcd4linux.bb
-rm -rf meta-openpli/recipes-openpli/images
