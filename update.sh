04d3549ae3ace362428796eda8edaae14d82d940
Persian Prince <persianpros@yahoo.com>
2019-08-05 21:29:00 +0430

    Be compatible with readymedia changes, see: https://github.com/OpenPLi/openpli-oe-core/commit/0424ef830b3ec62c271c9cc8660e2edf32c85155

--- update.sh
+++ update.sh
@@ -117,9 +117,7 @@ do
     echo "BBLAYERS_append = \" ${SCRIPTPATH}/${i}\"" >> ${BUILDDIR}/conf/bblayers.conf
 done
 echo "BBLAYERS_append = \" ${SCRIPTPATH}\"" >> ${BUILDDIR}/conf/bblayers.conf
-rm -rf meta-openpli/recipes-connectivity/minidlna
 rm -rf meta-openpli/recipes-extended/tzdata
-rm -rf meta-openpli/recipes-multimedia/readymedia
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnabrowser.bb
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-dlnaserver.bb
 rm -rf meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-lcd4linux.bb
